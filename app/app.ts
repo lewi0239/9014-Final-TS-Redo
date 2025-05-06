812d8ec08b58c6ab8dde1975160fd5d2c97c9887
lewi0239 <lewi0239@algonquinlive.com>
2025-05-06 13:38:12 -0400

    working on initial fetch

--- app/app.ts
+++ app/app.ts
@@ -1,21 +1,28 @@
-require("dotenv").config();
+import dotenv from "dotenv";
 dotenv.config();
-
-const URL = process.env.URL;
-const API = process.env.API;
-//import functions
-
 //load application when app is loaded
 document.addEventListener("DOMContentLoaded", app);
-
-//Define html elements
+//Declaring global variables
+const URL = process.env.URL;
+const API = process.env.API;
+const searchImage: string = "";
+const imageType: string = "photo";
+//import functions
 
 //runs web application when dom is loaded
 function app() {
   console.log("app is running..");
+  getImages(searchImage);
 }
 
 //fetching images for api to detect faces
-function getImages(searchImage): void {}
+async function getImages(searchImage: string): Promise<void> {
+  const url = `${URL}?key=${API}&q=${encodeURIComponent(
+    searchImage
+  )}&image_type=${imageType}`;
+  try {
+    const response = await fetch(url);
+  } catch {}
+}
 
 //ForEach image detect face
